af5a92a0dccdc7f032483cec5c7ecda07f7bd859
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-08-18 08:45:50 +0530

    added Logger Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,22 @@
-import { createStore }  from 'redux'
+import { createStore, applyMiddleware }  from 'redux'
 import rootReducer from './rootReducer'
+// logger is basically locks information related to the redux store
+import logger from 'redux-logger'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 export default store
 
 /**
  * After creating redux store next step is to provide this store to our react application
  * this is where the react-redux library used, react-redux library exports a component called provider
  * look file app.js
+ */
+
+/**
+ * Use logger middleware:-
+ * 1. first step to install the package - npm i redux-logger
+ * 2. second step is to import it in our store.js file
+ * 3. 3rd step is to apply this logger middleware for that we first import applyMiddleware function
+ *    from redux then to the createStore function we pass in a second parameter applyMiddleware,
+ *    and what is our Middleware ? it is logger
  */
